3a2a1f7a1fee308beed889ec4a24e6c7cc14bbd9
giinie <giinie72@gmail.com>
2019-02-02 01:24:25 +0900

    Initial commit

---
+++ src/head_first_python/ch04/function_template.py
@@ -0,0 +1,7 @@
+def a_descriptive_name(optional_arguments):
+    """A documentation string"""
+    # Your function's code goes here.
+    # Your function's code goes here.
+    # Your function's code goes here.
+    optional_value = ''
+    return optional_value
