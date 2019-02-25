6c95da17ab0e6eeec743d5c0d0df0806c8461175
giinie <giinie72@gmail.com>
2019-02-26 04:49:38 +0900

    local test

--- src/head_first_python/ch05/webapp/vsearch4web.py
+++ src/head_first_python/ch05/webapp/vsearch4web.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
+# @app.route('/')
+# def hello() -> '302':
+#     return redirect('/entry')
 
 
 @app.route('/search4', methods=['POST'])
-def do_search(phrase: str = 'life, the universe, and everything', letters: str = 'eiru,') -> str:
-    return str(search4letters(phrase=phrase, letters=letters))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase=phrase, letters=letters))
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results, )
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
 
 
-app.run()
+app.run(debug=True)
