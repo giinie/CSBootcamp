e426bbd56cf1c9a1c2a7d29488f86b37281a4b70
giinie <giinie72@gmail.com>
2019-02-27 22:15:56 +0900

    local test

---
+++ src/head_first_python/ch07/webapp/vsearch4web.py
@@ -0,0 +1,75 @@
+import mysql.connector
+from flask import Flask, render_template, request, escape
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+    dbconfig = {'host': '127.0.0.1',
+                'user': 'vsearch',
+                'password': 'qwer1234',
+                'database': 'vsearchlogDB', }
+
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+
+    _SQL = """insert into log
+              (phrase, letters, ip, browser_string, results)
+              VALUES
+              (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res,))
+
+    conn.commit()
+    # _SQL = """select * from log"""
+    # cursor.execute(_SQL)
+    # for row in cursor.fetchall():
+    #     print(row)
+    cursor.close()
+    conn.close()
+
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase=phrase, letters=letters))
+    log_request(request, results)
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results, )
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                           the_title='Welcome to search4letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            # contents.append([])
+            # for item in line.split('|'):
+            #     contents[-1].append(escape(item))
+            contents.append(escape(line).split('|'))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents, )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/head_first_python/ch08/countfromby.py
@@ -0,0 +1,7 @@
+class CountFromBy:
+    def __init__(self, v: int = 0, i: int = 1) -> None:
+        self.val = v
+        self.incr = i
+
+    def increase(self) -> None:
+        self.val += self.incr
