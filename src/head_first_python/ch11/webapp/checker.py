ebab06f386a7c2cdfec864924c162514764bdbb0
giinie <giinie72@gmail.com>
2019-03-05 04:41:17 +0900

    local test

---
+++ src/head_first_python/ch11/webapp/checker.py
@@ -0,0 +1,13 @@
+from functools import wraps
+
+from flask import session
+
+
+def check_logged_in(func: object) -> object:
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'logged_in' in session:
+            return func(*args, **kwargs)
+        return 'You are NOT logged in.'
+
+    return wrapper
