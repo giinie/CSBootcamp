167c8ef8e4fe29f38df8f9fa8847b86dfc985dc4
giinie <giinie72@gmail.com>
2019-03-04 18:23:07 +0900

    local test

--- src/head_first_python/ch09/webapp/vsearch4web.py
+++ src/head_first_python/ch09/webapp/vsearch4web.py
@@ -1,25 +1,41 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request
 from vsearch import search4letters
+
+from DBcm import UseDatabase
 
 app = Flask(__name__)
 
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'vsearch',
+                          'password': 'qwer1234',
+                          'database': 'vsearchlogDB', }
+
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-        # print(req.form, file=log, end='|')
-        # print(req.remote_addr, file=log, end='|')
-        # print(req.user_agent, file=log, end='|')
-        # print(res, file=log)
+    """Log details of the web request and the results."""
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                  (phrase, letters, ip, browser_string, results)
+                  VALUES
+                  (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res,))
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+    """Extract the posted data; perform the search; return results."""
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase=phrase, letters=letters))
     log_request(request, results)
+
     return render_template('results.html',
                            the_title=title,
                            the_phrase=phrase,
@@ -30,20 +46,23 @@ def do_search() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
 
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            # contents.append([])
-            # for item in line.split('|'):
-            #     contents[-1].append(escape(item))
-            contents.append(escape(line).split('|'))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    """Display the contents of the log table as a HTML table."""
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results
+                  from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+
     return render_template('viewlog.html',
                            the_title='View Log',
                            the_row_titles=titles,
